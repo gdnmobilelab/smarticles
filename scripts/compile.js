a8ea815dd1d21ecf3c2e0e985719d2b96ce5f141
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-06-09 14:30:07 -0400

    add dev scripts and build html

--- scripts/compile.js
+++ scripts/compile.js
@@ -7,4 +7,6 @@ fs.removeSync('.build');
 fs.mkdirsSync('.build');
 
 var data = getData();
-
+var html = fs.readFileSync('src/templates/index.html', 'utf8');
+var template = handlebars.compile(html);
+fs.writeFileSync('.build/index.html', template({atoms: data}));
