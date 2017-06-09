8de853c731025b287c32a1a06cc78a343a3a3472
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-06-09 15:18:32 -0400

    create very basic versions of each atom

--- scripts/compile.js
+++ scripts/compile.js
@@ -9,13 +9,28 @@ fs.mkdirsSync('.build');
 
 var data = getData();
 var html = fs.readFileSync('src/templates/index.html', 'utf8');
-var template = handlebars.compile(html);
 
 var partials = glob.readdirSync('src/templates/**/*.html');
 partials.forEach(function (partial) {
-    var name = partial.replace(/^.*[\\\/]/, '').split('.')[0];
+    var name = partial.replace('src/templates/', '').split('.')[0];
+    console.log(name);
     var template = fs.readFileSync(partial, 'utf8');
     handlebars.registerPartial(name, template);
 });
+
+handlebars.registerHelper("switch", function(value, options) {
+  this._switch_value_ = value;
+  var html = options.fn(this); // Process the body of the switch block
+  delete this._switch_value_;
+  return html;
+});
+
+handlebars.registerHelper("case", function(value, options) {
+  if (value == this._switch_value_) {
+    return options.fn(this);
+  }
+});
+
+var template = handlebars.compile(html);
 
 fs.writeFileSync('.build/index.html', template({atoms: data}));
