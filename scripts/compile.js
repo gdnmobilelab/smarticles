989149cc428f326dba72fd6dd19050b56d19256e
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-06-09 14:43:59 -0400

    load partials

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,4 +1,5 @@
 var fs = require('fs-extra');
+var glob = require('glob-fs')({ gitignore: true });
 var sass = require('node-sass');
 var handlebars = require('handlebars');
 var getData = require('./data.js');
@@ -9,4 +10,12 @@ fs.mkdirsSync('.build');
 var data = getData();
 var html = fs.readFileSync('src/templates/index.html', 'utf8');
 var template = handlebars.compile(html);
+
+var partials = glob.readdirSync('src/templates/**/*.html');
+partials.forEach(function (partial) {
+    var name = partial.replace(/^.*[\\\/]/, '').split('.')[0];
+    var template = fs.readFileSync(partial, 'utf8');
+    handlebars.registerPartial(name, template);
+});
+
 fs.writeFileSync('.build/index.html', template({atoms: data}));
