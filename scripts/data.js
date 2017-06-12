b206b9c895c26cf2a966a8d7b44ba52b62d6c4f0
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-06-12 16:15:20 -0400

    order atoms and fix iframe rendering bug

--- scripts/data.js
+++ scripts/data.js
@@ -4,8 +4,27 @@ var config = require('./config.js').config;
 
 var data;
 
+function createTimeStamps() {
+    for (var i in data) {
+        var date = data[i].date.split('/');
+        data[i].timeStamp = new Date(date[1] + '/' + date[0] + '/' + date[2] + ' ' + data[i].time);
+    }
+}
+
+function sortByReverseDate() {
+    data.sort(function(a,b) {
+        console.log(a.timeStamp);
+        return b.timeStamp - a.timeStamp;
+    });
+}
+
 module.exports = function() {
     data = request('GET', config.dataUrl);
     data = JSON.parse(data.getBody('utf8'));
-    return data.sheets.Sheet1;
+    data = data.sheets.Sheet1;
+
+    createTimeStamps();
+    sortByReverseDate();
+
+    return data;
 };
