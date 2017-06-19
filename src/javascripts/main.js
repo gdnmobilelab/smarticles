f71d740769d11b2ce92576a14e0318cd35ec2175
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-06-19 16:05:45 -0400

    remove days from html and js

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,5 @@
-var filter = require('./modules/filter');
 var groups = require('./modules/groups');
 var timeStamps = require('./modules/timeStamps');
 
-filter.init();
 groups.init();
 timeStamps.init();
