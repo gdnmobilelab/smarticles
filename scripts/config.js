92fc2eca827e9802148d22f67c35dba4b5c59902
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-08-21 10:54:16 -0400

    display weighting

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     config: {
-        "dataUrl": "https://interactive.guim.co.uk/docsdata-test/1DenynpDfQ5o7lqMle-W_uXKVUHkwOTyBFC6P8AydMaE.json"
+        "dataUrl": "https://interactive.guim.co.uk/docsdata-test/1Pbevy_RTqETBvif0X-qP74YJ5mPu33Muld7in5klVB4.json"
     }
 }
