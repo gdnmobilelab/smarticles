2943579d59dd8c66669f934e9b66ce7e0edff39f
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-10-18 14:16:24 -0400

    Add warning banner

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
+var banner = require('./modules/banner');
 var lastUpdated = require('./modules/lastUpdated');
 var filter = require('./modules/filter');
 var groups = require('./modules/groups');
 var dynamicCopy = require('./modules/dynamicCopy');
 var notifications = require('./modules/notifications');
 
+banner.init();
 lastUpdated.init();
 filter.init();
 groups.init();
---
+++ src/javascripts/modules/banner.js
@@ -0,0 +1,13 @@
+var $ = require('../vendor/jquery.js');
+
+module.exports = {
+    init: function() {
+        this.checkForNotifications();
+    },
+
+    checkForNotifications: function() {
+        if (!'Notification' in self && !'serviceWorker' in navigator) {
+            $('.banner').show();
+        }
+    }
+}
