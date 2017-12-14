2ae101b72e2b2ab5c9c419ae461ab23f4d8eada9
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-12-14 17:41:07 -0500

    change the way collapsed groups work so they don't fall apart more often

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     config: {
-        "id": "1MNeArI2LGLOb73h7UeujzzOrvUyBSfsObuguvSa8ju0"
+        "id": "1pILurzRdRG1UvrbbeN3AZKUeNGxirpt0nSBwqxkksS4"
     }
 }
--- src/javascripts/modules/groups.js
+++ src/javascripts/modules/groups.js
@@ -23,21 +23,19 @@ module.exports = {
         $(el).addClass('group--collapsed');
         $(el).removeClass('group--expanded');
 
+        var atoms = 0;
+
         $(el).children('.atom').each(function(i, el) {
             if (i !== 0) {
-                $(el).css({
-                    'margin-top': '-' + ($(el).height() + 4) + 'px',
-                    'z-index': '-' + i
-                });
                 $(el).attr('data-shrink', i);
             } else {
-                $(el).css({
-                    'margin-top': '',
-                    'z-index': ''
-                });
                 $(el).attr('data-shrink', '');
             }
+
+            atoms++;
         });
+
+        $(el).children('.atom').first().attr('style', 'margin-bottom: -24px');
     },
 
     expandGroup: function(el) {
@@ -49,11 +47,7 @@ module.exports = {
 
         analytics.send('Atom engagment', 'Expand', $firstAtom.attr('data-id'), '', $firstAtom.attr('data-weight'), $firstAtom.attr('data-type'));
 
-        $group.find('.atom').each(function(i, el) {
-            $(el).css({
-                'margin-top': ''
-            });
-            $(el).attr('data-shrink', '');
-        });
+        $group.children('.atom').first().attr('style', '');
+
     }
 }
