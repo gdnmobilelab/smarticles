11cc021415bb7d08a74badb3f003ae92ac3a427e
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-12-19 11:51:53 -0500

    track outgoing links

--- src/javascripts/modules/analytics.js
+++ src/javascripts/modules/analytics.js
@@ -6,10 +6,19 @@ var id = 'UA-77348538-9',
 module.exports = {
     init: function() {
         this.setValues();
+        this.bindings();
     },
 
     setValues: function() {
         atomsPresented = $('.atom').length;
+    },
+
+    bindings: function() {
+        $('a.is-tracked').click(function(e) {
+            e.preventDefault();
+            var url = $(e.currentTarget).attr('href');
+            this.externalLink(url);
+        }.bind(this));
     },
 
     send: function(category, action, label, value = 0, weight = null, type = null) {
@@ -20,5 +29,13 @@ module.exports = {
             'dimension3': weight,
             'dimension4': type,
         })
+    },
+
+    externalLink: function(url) {
+        window.ga('send', 'event', 'outbound', 'click', url, {
+            'transport': 'beacon',
+            'hitCallback': function() { document.location = url; }
+        });
     }
 }
+
