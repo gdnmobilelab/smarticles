09dfc2e53815faf53c400442e12da95255022fdb
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-01-24 17:39:10 +0000

    remove console log of local API

--- scripts/compile.js
+++ scripts/compile.js
@@ -14,8 +14,6 @@ fs.mkdirsSync('.build');
 var useLocalAPI = process.argv.slice(2);
 var data = getData(useLocalAPI);
 
-console.log(useLocalAPI);
-
 // HTML
 var html = fs.readFileSync('src/templates/index.html', 'utf8');
 var partials = glob.readdirSync('src/templates/**/*.html');
